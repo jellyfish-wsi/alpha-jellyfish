b56637f2ccefb1310275424126fb4f65d116fa88
Dasha Day Hisoler <dashadayhisoler@gmail.com>
2020-03-30 16:49:15 -0500

    Implement connect-ensure-login and pass user information to flight.pug

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var flight = require('../lib/flight-data');
+var connect = require('connect-ensure-login');
 var router = express.Router();
 
 /* GET home page. */
@@ -7,22 +8,26 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Travel Itinerary' });
 });
 
-router.get('/flight', function(req, res, next) {
-  res.render('flight', {title: 'Flight Information'});
-});
+router.get('/flight',
+  connect.ensureLoggedIn('/auth'),
+  function(req, res, next) {
+    res.render('flight', {title: 'Flight Information', user: req.user});
+  });
 
-router.post('/flight', async function(req, res, next) {
-  let flightNumber = req.body.flightNumber;
-  let departureDate = req.body.depDate;
+router.post('/flight',
+  connect.ensureLoggedIn('/auth'),
+  async function(req, res, next) {
+    let flightNumber = req.body.flightNumber;
+    let departureDate = req.body.depDate;
 
-  try {
-      let async_data = await flight.getFlightData(flightNumber, departureDate);
-      res.render('flight', {title: 'Flight Information', async_data});
+    try {
+        let async_data = await flight.getFlightData(flightNumber, departureDate);
+        res.render('flight', {title: 'Flight Information', user: req.user, async_data});
 
-    } catch (err) {
-      res.status(err.response.status)
-      console.log("Caught error! " + err.message);
-    }
-});
+      } catch (err) {
+        res.status(err.response.status)
+        console.log("Caught error! " + err.message);
+      }
+  });
 
 module.exports = router;
